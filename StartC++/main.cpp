0b3804df3b49d27815a16c2add9a7d1b9631bb29
Murat Çelik <muratclik2@gmail.com>
2023-03-06 13:57:46 +0300

    first commit

--- StartC++/main.cpp
+++ StartC++/main.cpp
@@ -1,14 +1,29 @@
-//
-//  main.cpp
-//  StartC++
-//
-//  Created by Murat Çelik on 6.03.2023.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+
+class dikdörtgen{
+public:
+    int en;
+    int boy;
+    int alan(){
+        return en*boy;
+    }
+    int cevre(){
+        return 2*(en+boy);
+    }
+};
+
+int main(){
+    dikdörtgen dikdörtgen1;
+    dikdörtgen dikdörtgen2;
+    dikdörtgen1.en = 5;
+    dikdörtgen1.boy = 2;
+    dikdörtgen2.en = 3;
+    dikdörtgen2.boy = 8;
+    
+    cout<<"1. dikdörtgenin alanı: "<<dikdörtgen1.alan()<<"    cevresi: "<<dikdörtgen1.cevre();
+    cout<<"\n2. dikdörtgenin alanı: "<<dikdörtgen2.alan()<<"    cevresi: "<<dikdörtgen2.cevre();
+    
 }
